cbabab0024e546ca3130eed471c9b31d837c0a36
Nodirbek Ibragimov <59182238+nibragimov@users.noreply.github.com>
2020-10-26 22:29:23 +0100

    test inserts

--- project/input_form_members.php
+++ project/input_form_members.php
@@ -13,38 +13,40 @@
     }
     echo $member_exp_date;
         
-    $stmt_member = $conn->prepare("INSERT INTO members (mail, members_name, members_expiry_date) VALUES (?, ?, FROM_UNIXTIME(?));");
-    $stmt_member->bind_param('ssi', $member_mail, $member_name, $member_expiry_date);
-    if(!$stmt_member->execute()){
-        header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=members");
-        $stmt_member->close();
-        exit();
-    }
+    //$stmt_member = $conn->prepare("INSERT INTO members (mail, members_name, members_expiry_date) VALUES (?, ?, FROM_UNIXTIME(?));");
+
+    $stmt_member = $conn->query("INSERT INTO members (mail, members_name, members_expiry_date) VALUES (jim@dmx.com,Jim Finn,FROM_UNIXTIME($member_exp_date));");
+    // $stmt_member->bind_param('ssi', $member_mail, $member_name, $member_expiry_date);
+    // if(!$stmt_member->execute()){
+    //     header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=members");
+    //     $stmt_member->close();
+    //     exit();
+    // }
     $stmt_member->close();
 
-    if($membership_type == "gym_member"){
-        $stmt_gym = $conn->prepare("INSERT INTO gym_members (members_mail) VALUES (?);");
-        $stmt_gym->bind_param('s', $member_mail);
-        if(!$stmt_gym->execute()){
-            header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=gym_members");
-            $stmt_gym->close();
-            exit();
-        }
-        else{
-            header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=success&table=gym_members_and_members");
-        }
-        $stmt_gym->close();
-    }
-    else if($membership_type == "pool_member"){
-        $stmt_pool = $conn->prepare("INSERT INTO pool_members (members_mail) VALUES (?);");
-        $stmt_pool->bind_param('s', $member_mail);
-        if(!$stmt_pool->execute()){
-            header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=pool_members");
-            $stmt_pool->close();
-            exit();
-        }else{
-            header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=success&table=pool_members_and_members");
-        }
-        $stmt_pool->close();
-    }
+    // if($membership_type == "gym_member"){
+    //     $stmt_gym = $conn->prepare("INSERT INTO gym_members (members_mail) VALUES (?);");
+    //     $stmt_gym->bind_param('s', $member_mail);
+    //     if(!$stmt_gym->execute()){
+    //         header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=gym_members");
+    //         $stmt_gym->close();
+    //         exit();
+    //     }
+    //     else{
+    //         header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=success&table=gym_members_and_members");
+    //     }
+    //     $stmt_gym->close();
+    // }
+    // else if($membership_type == "pool_member"){
+    //     $stmt_pool = $conn->prepare("INSERT INTO pool_members (members_mail) VALUES (?);");
+    //     $stmt_pool->bind_param('s', $member_mail);
+    //     if(!$stmt_pool->execute()){
+    //         header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=pool_members");
+    //         $stmt_pool->close();
+    //         exit();
+    //     }else{
+    //         header("http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=success&table=pool_members_and_members");
+    //     }
+    //     $stmt_pool->close();
+    // }
     $conn->close();
