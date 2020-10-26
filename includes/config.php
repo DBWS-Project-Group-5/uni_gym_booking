e9e86c4ca49e0b6aa0b85bb45f2953006e35756c
Nodirbek Ibragimov <59182238+nibragimov@users.noreply.github.com>
2020-10-26 22:09:35 +0100

    configured settings

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,15 @@
 <?php
-define("DB_HOST",'10.72.1.14');
-define("DB_USERNAME",'group5');
-define("DB_PASSWORD",'hz2aAr');
+// define("DB_HOST",'10.72.1.14');
+// define("DB_USERNAME",'group5');
+// define("DB_PASSWORD",'hz2aAr');
+$servername = '10.72.1.14';
+$username = "group5";
+$password = "hz2aAr";
+$dbname = "group5";
 date_default_timezone_set("Europe/Berlin");
 
 
-$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error){
     die("Connection failed: ".$conn->connect_error);
