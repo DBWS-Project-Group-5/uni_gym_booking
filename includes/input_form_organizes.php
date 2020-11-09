4f208fcf87386160e754a369cfd2775aa1f8f8be
Nodirbek Ibragimov <59182238+nibragimov@users.noreply.github.com>
2020-11-09 23:50:52 +0100

    fixed some things

--- includes/input_form_organizes.php
+++ includes/input_form_organizes.php
@@ -1,20 +1,24 @@
 <?php
     require('../includes/config.php');
 
-    $stmt_event = $conn->prepare("SELECT * FROM event WHERE event_name=?;");
+    $stmt_event = $conn->prepare("SELECT * FROM event WHERE event_name=?");
     $stmt_event->bind_param("s", $event_name);
     $event_name = $_POST['event_name'];
     
     $stmt_event->execute();
     $result = $stmt_event->get_result();
+    $event = $result->fetch_assoc();
+    
+    while($row = $result->fetch_assoc()){
+        $event_id = (int)$row['id'];
+    }
     $stmt_event->close();
-
+    
     $stmt_organizes = $conn->prepare("INSERT INTO organizes (staff_mail, event_id) VALUES (?, ?);");
     $stmt_organizes->bind_param('si', $staff_mail, $event_id);
     $staff_mail = $_POST['staff_mail'];
-    while($row = $result->fetch_assoc()){
-       $event_id = (int)$row['id'];
-    }
+
+    
     
     if(!$stmt_organizes->execute()){
         header("location:http://clabsql.clamv.jacobs-university.de/~nibragimov/uni_gym_booking/project/status.php?status=error&table=organizes");
